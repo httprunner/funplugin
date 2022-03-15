ddcbacbdd1256852a309a6e1c6454e507411008a
debugtalk <mail@debugtalk.com>
2022-03-15 19:39:24 +0800

    fix: unittests

--- python/examples/debugtalk.py
+++ python/examples/debugtalk.py
@@ -1,8 +1,9 @@
 import logging
 import sys
+import os
 from typing import List
 
-sys.path.insert(0, "/Users/debugtalk/MyProjects/HttpRunner-dev/plugin/python/")
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import plugin
 
