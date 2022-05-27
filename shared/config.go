62b4f2066c5d0dd7f0e4f6412eda569ecee73216
xucong053 <xucong053@outlook.com>
2022-05-27 13:54:18 +0800

    bump version to v0.4.7

--- shared/config.go
+++ shared/config.go
@@ -4,7 +4,7 @@ import (
 	"github.com/hashicorp/go-plugin"
 )
 
-const Version = "v0.4.6"
+const Version = "v0.4.7"
 
 // PluginTypeEnvName is used to specify hashicorp go plugin type, rpc/grpc
 const PluginTypeEnvName = "HRP_PLUGIN_TYPE"
